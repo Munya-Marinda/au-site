95a3ea100de7eb1b3a5985e91ab023ea339ce232
Munya Marinda <moyomarinda@gmail.com>
2023-09-30 03:15:28 +0200

    Minor style changes.

--- packages/african_union_1/src/components/components/SocialMediaIcons.js
+++ packages/african_union_1/src/components/components/SocialMediaIcons.js
@@ -12,16 +12,16 @@ const SocialMediaIcons = ({ state }) => {
   return (
     <div className="topbar_socialLinks_parent_1">
       <Link link="#twitter" className="twitter_icon_parent_1">
-        <FaTwitter />
+        <FaTwitter color="white" />
       </Link>
       <Link link="#facebook" className="facebook_icon_parent_1">
-        <FaFacebookF />
+        <FaFacebookF color="white" />
       </Link>
       <Link link="#youtube" className="youtube_icon_parent_1">
-        <FaYoutube />
+        <FaYoutube color="white" />
       </Link>
       <Link link="#linkedIn" className="linkedIn_icon_parent_1">
-        <FaLinkedinIn />
+        <FaLinkedinIn color="white" />
       </Link>
     </div>
   );
